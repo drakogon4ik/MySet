970c973eec7518574973d1e8e696e9aad4b36223
drakogon4ik <94756197+drakogon4ik@users.noreply.github.com>
2023-12-14 22:51:15 +0200

    clear

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,13 +14,11 @@ namespace ConsoleApp1
         {
             int[] arr = { 1, 7, 3, -100, 40, 2, -1, 7, 80, 7 };
             MySet node = TurnIntoChain(arr);
-            Insert(node, 7);
+            node.InsertTo(7);
             Console.WriteLine(node);
-            if (BelongTo(node, 7))
-                Console.WriteLine("true");
-            else 
-                Console.WriteLine("false");
+            Console.WriteLine(node.BelongTo(5));
         }
+
 
         static MySet TurnIntoChain(int[] arr)
         {
@@ -32,35 +30,10 @@ namespace ConsoleApp1
             return node;
         }
 
-        static bool BelongTo(MySet node, int num)
-        {
-            if (node == null)
-                return false;
-            else if (node.GetInfo() == num)
-                return true;
-            if (BelongTo(node.GetNext(), num))
-                return true;
-            else
-                return false;
-        }
-
-        static void Insert(MySet node, int num)
-        {
-            MySet comming = new MySet(num, null);
-            if (node == null)
-                node = comming;
-            else if (node.GetNext() == null)
-            {
-                comming.SetNext(node.GetNext());
-                node.SetNext(comming);
-            }
-            else
-            {
-                Insert(node.GetNext(), num);
-            }
-        }
 
     }
+
+
     class MySet
     {
         private int info;
@@ -105,10 +78,46 @@ namespace ConsoleApp1
         }
 
 
-        
+        private void Insert(MySet node, int num)
+        {
+            MySet comming = new MySet(num, null);
+            if (node == null)
+                node = comming;
+            else if (node.GetNext() == null)
+            {
+                comming.SetNext(node.GetNext());
+                node.SetNext(comming);
+            }
+            else
+            {
+                Insert(node.GetNext(), num);
+            }
+        }
+
+        public void InsertTo(int num)
+        {
+            MySet node = new MySet(info, next);
+            Insert(node, num);
+        }
 
 
-       
+        private bool Belong(MySet node, int num)
+        {
+            if (node == null)
+                return false;
+            else if (node.GetInfo() == num)
+                return true;
+            if (Belong(node.GetNext(), num))
+                return true;
+            else
+                return false;
+        }
+
+        public bool BelongTo(int num)
+        {
+            MySet node = new MySet(info, next);
+            return Belong(node, num);
+        }
 
 
         public override string ToString()
@@ -116,6 +125,5 @@ namespace ConsoleApp1
 
             return this.info + " " + this.next;
         }
-
     }
 }
